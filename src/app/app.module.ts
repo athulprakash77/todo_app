b23c0211592dedbbc0815d2a555b1a51fd14fc91
athulprakash007 <athull362@gmail.com>
2022-11-17 16:12:23 -0800

    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TodoallComponent } from './todoall/todoall.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+
+const myroutes:Routes=[
+
+  {path:"",component:TodoComponent},
+  {path:"todoall",component: TodoallComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    NavbarComponent,
+    TodoallComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroutes)
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
